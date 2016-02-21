7eb0eba0a79c63069079ee3cb941572e7740f5c3
chrismlusk <chris.m.lusk@gmail.com>
2016-02-21 13:05:24 -0500

    Change compare_dicts() function

--- scrape.py
+++ scrape.py
@@ -1,79 +1,66 @@
 import requests
 from bs4 import BeautifulSoup
-import re
 import json
 import os
 
-url = 'http://www.ncaa.com/scoreboards/basketball-men/d1'
-response = requests.get(url)
-html = response.content
-soup = BeautifulSoup(html, 'lxml')
-scoreboard = soup.find(id='scoreboard')
 
-dict_of_games = {}
-is_posted = False
-games = scoreboard.find_all('section', class_='game')
-# --- Iterate and scrape data for games that are final.
-for game in games:
-    if game.find('div', class_='final') is None:
-        continue
-    game_id = game['id']
-    pattern = r'^(.*[\\\/])'  # --- Find all chars before last slash.
-    key = re.sub(pattern, '', game_id)
-    # --- Each dict has a key of the id scraped from the table.
-    dict_of_games[key] = {
-        'away': {
-            'team': None,
-            'score': None
-        },
-        'home': {
-            'team': None,
-            'score': None
-        },
-        'is_posted': False
-    }
-    rows = game.find_all('tr')[1:]  # --- Skip the first row in table.
-    for row in rows:
-        cells = row.find_all('td', {'class': ['school', 'final']})
-        if rows.index(row) == 0:  # --- Index 0 is away team.
-            for cell in cells:
-                if 'school' in cell.attrs['class']:
-                    team = cell.find('a').text.strip()
-                    dict_of_games[key]['away']['team'] = team
-                elif 'final' in cell.attrs['class']:
-                    score = cell.text.strip()
-                    dict_of_games[key]['away']['score'] = score
-        elif rows.index(row) == 1:  # --- Index 1 is home team.
-            for cell in cells:
-                if 'school' in cell.attrs['class']:
-                    team = cell.find('a').text.strip()
-                    dict_of_games[key]['home']['team'] = team
-                elif 'final' in cell.attrs['class']:
-                    score = cell.text.strip()
-                    dict_of_games[key]['home']['score'] = score
+def get_data():
+    url = 'http://www.ncaa.com/scoreboards/basketball-men/d1'
+    soup = BeautifulSoup(requests.get(url).content, 'lxml')
+    return soup.find(id='scoreboard')
 
-file_path = 'data/results.json'
-try:
-    # --- If there is no data in the JSON file, create a new one.
-    if os.stat(file_path).st_size == 0:
-        with open(file_path, 'wb') as f:
-            json.dump(dict_of_games, f)
-            print "New JSON file started."
-    else:
-        with open(file_path, 'rb+') as f:
-            saved_dict = json.load(f)
-            for item in dict_of_games.keys():
-                if item in saved_dict:
-                    dict_of_games.pop(item, None)
-            # --- If there are new game results, add them to the file.
-            if dict_of_games:
-                new_items = json.dumps(dict_of_games).replace('{', ',', 1)
-                f.seek(-2, 2)  # --- Move backward to add items inside {}
-                f.write(new_items)
-                print "New results added to JSON file."
-            # --- Otherwise, don't change the file.
+
+def get_date():
+    date = get_data().find('h2').text.split(',')[1].replace(' ', '')
+    return date
+
+
+def parse_row(row):
+    team = row.find_all('td', {'class': ['school']})[0].find('a').text.strip()
+    score = row.find_all('td', {'class': ['final']})[0].text.strip()
+    return team, score
+
+
+def compare_dicts(new_dict):
+    file_path = 'data/' + get_date() + '.json'
+    if os.path.exists(file_path):
+        with open(file_path, 'r+') as f:
+            json_file = json.load(f)
+            for key in json_file:
+                if key in new_dict:
+                    del new_dict[key]
+            if new_dict:
+                updated_dict = json_file.copy()
+                updated_dict.update(new_dict)
+                f.seek(0)
+                json.dump(updated_dict, f, indent=4, separators=(',', ': '))
+                print "New results to add."
             else:
                 print "Nothing new right now."
+    else:
+        if new_dict:
+            with open(file_path, 'w') as f:
+                json.dump(new_dict, f, indent=4, separators=(',', ': '))
+                print "New JSON file started."
+        else:
+            print "No results yet."
 
-except OSError as e:
-    print e
+
+def main():
+    results = {}
+    is_posted = False
+    for game in get_data().find_all('section', class_='game'):
+        if game.find('div', class_='final') is None:
+            continue
+        key = game['id'].split('/')[-1]
+        away_team, away_score = parse_row(game.find_all('tr')[1])
+        home_team, home_score = parse_row(game.find_all('tr')[2])
+        results[key] = [away_team, away_score,
+                        home_team, home_score,
+                        is_posted]
+
+    compare_dicts(results)
+
+
+if __name__ == '__main__':
+    main()
